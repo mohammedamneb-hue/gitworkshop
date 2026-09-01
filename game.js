06d929e8064f586c0bf95a6ed8d69fae5c5e69fa
mohammedamneb-hue <mohammedamneb@gmail.com>
2026-09-01 09:41:35 +0100

    firstgamecommit

---
+++ game.js
@@ -0,0 +1,10 @@
+const playername = "yash"
+let level = 11
+let age = 19
+let score = 50
+const country = "morocco"
+console.log(playername)
+console.log(level)
+console.log(age)
+console.log(score)
+console.log(country)
